dc139bf8e8403a85047cf9c396b82ca73229dd36
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2023-06-25 11:42:32 -0300

    test: produtos repository - os objetos listados devem ser instancias da classe Produto

--- src/infra/database/in-memory/produtos-repository.spec.ts
+++ src/infra/database/in-memory/produtos-repository.spec.ts
@@ -1,4 +1,5 @@
 import { IProdutosRepository } from "../../../application/repositories/produtos-repository";
+import { Produto } from "../../../domain/entities/produto";
 import { InMemoryProdutosRepository } from "./produtos-repository";
 
 describe("In memory produtos repository", () => {
@@ -19,5 +20,13 @@ describe("In memory produtos repository", () => {
 		const produtos = await sut.listar();
 
 		expect(Array.isArray(produtos)).toBe(true);
+	});
+
+	test("Os objetos listados devem ser instâncias da classe Produto", async () => {
+		const { sut } = makeSut();
+
+		const produtos = await sut.listar();
+
+		expect(produtos[0] instanceof Produto).toBe(true);
 	});
 });
