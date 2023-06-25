9dd32695dddab417999c612d33a786b54940f347
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2023-06-25 12:15:19 -0300

    config: preparar middlewares de body-parser content-type e cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { router } from "../routes/routes";
+import { bodyParser } from "../middlewares/body-parser";
+import { contentType } from "../middlewares/content-type";
+import { cors } from "../middlewares/cors";
 dotenv.config();
 
 export class App {
@@ -9,12 +12,19 @@ export class App {
 
 	constructor() {
 		this.express = express();
+		this.middlewares();
 		this.router();
 		this.listen();
 	}
 
 	private router() {
 		this.express.use(router);
+	}
+
+	private middlewares() {
+		this.express.use(bodyParser);
+		this.express.use(contentType);
+		this.express.use(cors);
 	}
 
 	private listen() {
